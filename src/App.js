fae0a223c81190bec7d2e7224ec891ce56ba1940
Zasada94 <pawelzasada2@wp.pl>
2023-07-13 11:09:39 +0200

    posts display and getPosts function added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 
 const Container = styled.div`
@@ -19,8 +20,8 @@ const Input = styled.input`
 	padding: 8px 10px;
 	font-size: 16px;
 	margin: 10px 0;
-  border-radius: 10px;
-  border: 1px solid black;
+	border-radius: 10px;
+	border: 1px solid black;
 `;
 
 const Button = styled.button`
@@ -43,16 +44,42 @@ const PostTitle = styled.h2``;
 const PostBody = styled.p``;
 
 function App() {
+	const [searchInput, setSearchInput] = useState("");
+	const [results, setResults] = useState([]);
+
+	const getPosts = () => {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then((response) => response.json())
+			.then((posts) => {
+				const filteredPosts = posts.filter((post) =>
+					post.title.toLowerCase().includes(searchInput.toLowerCase())
+				);
+
+				filteredPosts.sort((a, b) => b.title.length - a.title.length);
+				setResults(filteredPosts);
+			})
+			.catch((error) => {
+				console.log("Error fetching posts:", error);
+			});
+	};
+
 	return (
 		<Container>
 			<Title>Blog Posts Search</Title>
-			<Input type="text" placeholder="Search by title..."></Input>
-			<Button>Search</Button>
+			<Input
+				type="text"
+				placeholder="Search by title..."
+				value={searchInput}
+				onChange={(e) => setSearchInput(e.target.value)}
+			></Input>
+			<Button onClick={getPosts}>SEARCH</Button>
 			<ResultsWrapper>
-				<Post>
-					<PostTitle>title</PostTitle>
-					<PostBody>body</PostBody>
-				</Post>
+				{results.map((post) => (
+					<Post key={post.id}>
+						<PostTitle>{post.title}</PostTitle>
+						<PostBody>{post.body}</PostBody>
+					</Post>
+				))}
 			</ResultsWrapper>
 		</Container>
 	);
