ae10d1ffd1408b4d7c30ac1cb82b3f31c6321d98
Zasada94 <pawelzasada2@wp.pl>
2023-07-13 12:35:04 +0200

    rwd finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { styled } from "styled-components";
+import { mobile, tablet } from "./responsive";
 
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	text-align: center;
+	overflow: hidden;
 	background: linear-gradient(
 		0deg,
 		rgba(255, 255, 255, 0) 0%,
@@ -13,35 +15,82 @@ const Container = styled.div`
 	);
 	min-height: 100vh;
 	padding: 30px 0;
+	${mobile({
+		padding: "20px 0",
+	})}
 `;
-const Title = styled.h1``;
+const Title = styled.h1`
+	font-size: 36px;
+	${tablet({
+		fontSize: "32px",
+	})}
+`;
 
 const Input = styled.input`
 	padding: 8px 10px;
 	font-size: 16px;
-	margin: 10px 0;
+	margin: 15px 30px;
 	border-radius: 10px;
 	border: 1px solid black;
+	max-width: 80vw;
 `;
 
 const Button = styled.button`
+	font-weight: 700;
 	padding: 8px 16px;
-	margin-bottom: 10px;
+	margin-bottom: 15px;
 	background-color: white;
 	color: black;
-	border: none;
+	border: 1px solid rgba(0, 0, 0, 0);
 	cursor: pointer;
 	box-shadow: 5px 5px 0px 0px rgb(0, 0, 0);
 	border-radius: 10px;
+
+	&:hover {
+		background-color: #dadada;
+		border: 1px solid black;
+		color: #3b444b;
+		text-shadow: -1px -1px 0 #cdcdcd;
+		transition: all 250ms linear;
+		box-shadow: 4px 4px 0px 0px rgb(0, 0, 0);
+	}
 `;
 
-const ResultsWrapper = styled.div``;
+const ResultsWrapper = styled.div`
+	max-width: 1200px;
+	margin: 0 100px;
+	${mobile({
+		margin: "0 30px",
+	})}
+		${tablet({
+		margin: "0 50px",
+	})}
+`;
 
-const Post = styled.div``;
+const Post = styled.div`
+	margin-bottom: 20px;
+	text-align: left;
+`;
 
-const PostTitle = styled.h2``;
+const PostTitle = styled.h2`
+	margin-bottom: 5px;
+	${mobile({
+		fontSize: "18px",
+	})}
+	${tablet({
+		fontSize: "22px",
+	})}
+`;
 
-const PostBody = styled.p``;
+const PostBody = styled.p`
+	font-size: 17px;
+	${mobile({
+		fontSize: "15px",
+	})}
+	${tablet({
+		fontSize: "16px",
+	})}
+`;
 
 function App() {
 	const [searchInput, setSearchInput] = useState("");
